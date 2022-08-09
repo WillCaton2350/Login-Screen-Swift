6ac32d1b6af15419f5afc86e6901fad2bf2ef4c3
WillCatonJr <WillCatonJr@Gmail.com>
2022-08-09 07:54:44 -0500

    Initial Commit

---
+++ Login Screen/Login_ScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_ScreenApp.swift
+//  Login Screen
+//
+//  Created by Mini on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Login_ScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
